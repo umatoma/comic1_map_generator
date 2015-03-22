710c19cabf18cc9d7c24fc88a756d0ce6bdb9cf2
umatoma <umatomakun@gmail.com>
2015-03-22 15:39:57 +0900

    画像サイズ調整

--- map_generator.rb
+++ map_generator.rb
@@ -4,11 +4,11 @@ include Magick
 
 LAYOUTS_CSV_FILE_NAME = 'layouts.csv'
 BLOCKS_CSV_FILE_NAME = 'blocks.csv'
-IMAGE_WIDTH = 1270
-IMAGE_HEIGHT = 540
 IMAGE_BACKGROUND_COLOR = 'white'
 IMAGE_FILE_NAME = 'image.png'
 
+BOOTH_WIDTH = 13
+BOOTH_HEIGHT = 13
 BOOTH_FILL = 'white'
 BOOTH_STROKE = 'black'
 
@@ -17,7 +17,9 @@ SPACE_NO_POINT_SIZE = 9
 layouts = CSV.table(LAYOUTS_CSV_FILE_NAME)
 blocks = CSV.table(BLOCKS_CSV_FILE_NAME)
 
-image = Image.new(IMAGE_WIDTH, IMAGE_HEIGHT) {
+image_w = (layouts.map { |x| x[:pos_x] }.max + 1) * BOOTH_WIDTH
+image_h = (layouts.map { |x| x[:pos_y] }.max + 1) * BOOTH_HEIGHT
+image = Image.new(image_w, image_h) {
   self.background_color = IMAGE_BACKGROUND_COLOR
 }
 
@@ -31,23 +33,19 @@ space_no_gc = Draw.new
 space_no_gc.pointsize(SPACE_NO_POINT_SIZE)
 space_no_gc.pointsize = SPACE_NO_POINT_SIZE
 
-layout_max_x = layouts.map { |x| x[:pos_x] }.max
-layout_max_y = layouts.map { |x| x[:pos_y] }.max
-booth_w = IMAGE_WIDTH / layout_max_x
-booth_h = IMAGE_HEIGHT / layout_max_y
 layouts.each do |layout|
   ## Draw circle booth frame
-  x1 = layout[:pos_x] * booth_w
-  y1 = layout[:pos_y] * booth_h
-  x2 = x1 + booth_w
-  y2 = y1 + booth_h
+  x1 = layout[:pos_x] * BOOTH_WIDTH
+  y1 = layout[:pos_y] * BOOTH_HEIGHT
+  x2 = x1 + BOOTH_WIDTH
+  y2 = y1 + BOOTH_HEIGHT
   booths_gc.rectangle(x1, y1, x2, y2)
 
   ## Draw circle booth space number
   space_no = layout[:space_no].to_s
   metrics = space_no_gc.get_type_metrics(space_no)
-  x = x1 + (booth_w - metrics.width) * 0.5
-  y = y2 - (booth_h - SPACE_NO_POINT_SIZE) * 0.5
+  x = x1 + (BOOTH_WIDTH - metrics.width) * 0.5
+  y = y2 - (BOOTH_HEIGHT - SPACE_NO_POINT_SIZE) * 0.5
   space_no_gc.text(x, y, space_no)
 end
 
